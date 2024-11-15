5d3553369140334d0963655f639c27d2aafd1779
karurac4 <ckarura4@gmail.com>
2024-11-15 14:14:31 +0900

    first commit

--- error_sample/sample.php
+++ error_sample/sample.php
@@ -1,1 +1,24 @@
-<?
+<?php
+// エラー解消用の資料
+
+//1 セミコロン忘れ
+echo "Wellcome to TheNewGate"
+
+
+//2 引数の不足
+function add($a, $b) {
+    return $a + $b;
+}
+
+echo add(5);
+
+//3 定義し忘れ
+$array = ["name" => "Hiro"];
+echo $array["age"];
+
+
+//4 文字連結間違い
+echo "Wellcome to " + "TheNewGate";
+
+
+?>
